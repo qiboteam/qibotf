4bea64bba17b33c0b072e327a2f905938498da67
Stefano Carrazza <stefano.carrazza@cern.ch>
2021-05-20 21:08:09 +0200

    adding ld library path to cuda 11.2

--- .github/scripts/build-linux.sh
+++ .github/scripts/build-linux.sh
@@ -29,7 +29,7 @@ else
     exit 1
 fi
 
-# install cuda-11.0
+# install cuda-11.2
 curl https://developer.download.nvidia.com/compute/cuda/repos/rhel7/x86_64/cuda-repo-rhel7-10.2.89-1.x86_64.rpm -o cuda-repo.rpm
 rpm -i cuda-repo.rpm
 retry yum install -y cuda-compiler-11-2 cuda-libraries-devel-11-2
@@ -37,6 +37,7 @@ retry yum install -y cuda-compiler-11-2 cuda-libraries-devel-11-2
 # set env variables
 export PYTHON=$PYBIN/python
 export CUDA_PATH=/usr/local/cuda-11.2/
+export LD_LIBRARY_PATH=/usr/local/cuda-11.2/lib64/:$LD_LIBRARY_PATH
 
 # build wheel
 retry $PYTHON -m pip install -r requirements.txt
